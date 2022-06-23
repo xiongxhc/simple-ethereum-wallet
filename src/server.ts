c8c205f04ea93bf386c6745140f83a900c9ff528
Chris Xiong <xionghc713@gmail.com>
2022-06-23 18:49:54 +0800

    remove TODOs

--- src/server.ts
+++ src/server.ts
@@ -22,12 +22,6 @@ try {
 // Routes
 routes(app);
 
-// TODO: Register user
-
-// TODO: Get username and address
-
-// TODO: Get etherum balance and a specific token
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`>>>>> Server has started on port: ${PORT}`);
