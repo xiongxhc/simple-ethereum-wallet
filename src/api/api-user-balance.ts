925bce96a0d60bce6adcba6448d5cd564c99381d
Chris Xiong <xionghc713@gmail.com>
2022-06-24 02:30:50 +0800

    update api-user-balance

--- src/api/api-user-balance.ts
+++ src/api/api-user-balance.ts
@@ -6,17 +6,9 @@ import { unhandledException } from "../utils/unhandledException";
 const retrieveUserBalance = async (req: Request, res: Response) => {
   try {
     const { eth_address, erc_token } = req.query;
-    if (!eth_address || !erc_token) {
-      throw new APIError(`Invalid inputs`);
-    }
     const balance = await getBalance(eth_address, erc_token);
     return res.status(200).json(balance);
   } catch (err) {
-    if (err instanceof APIError) {
-      return res.status(500).json({
-        message: err.message,
-      });
-    }
     if (err instanceof GetBalanceError) {
       return res.status(500).json({
         message: err.message,
